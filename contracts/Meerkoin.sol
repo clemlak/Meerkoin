306b53f97104059d8233ec36ed10cf8a18845784
Clement Lakhal <clementlakhal@gmail.com>
2019-04-28 17:16:04 +0200

    Added meta transfer functions and related tests

--- contracts/Meerkoin.sol
+++ contracts/Meerkoin.sol
@@ -14,7 +14,7 @@ contract Meerkoin is ERC20, ERC20Detailed {
     string private _symbol = "MEER";
     uint8 private _decimals = 18;
 
-    uint256 public price = 0.01 ether;
+    mapping (address => uint256) public nonces;
 
     constructor() public ERC20Detailed(
         _name,
@@ -24,10 +24,62 @@ contract Meerkoin is ERC20, ERC20Detailed {
     }
 
     function buyTokens() external payable {
-        require(msg.value >= price, "Sent amount is too low");
+        _mint(msg.sender, msg.value);
+    }
 
-        uint256 amount = msg.value / price * 10 ** uint256(_decimals);
+    function sellTokens(uint256 amount) external {
+        _burn(msg.sender, amount);
+        msg.sender.transfer(amount);
+    }
 
-        _mint(msg.sender, amount);
+    function metaTransfer(bytes memory signature, address to, uint256 amount, uint256 nonce) public {
+        bytes32 hash = metaTransferHash(to, amount, nonce);
+        address signer = getSigner(hash, signature);
+
+        require(signer != address(0), "Cannot get signer");
+        require(nonce == nonces[signer], "Nonce is invalid");
+
+        nonces[signer] += 1;
+
+        _transfer(signer, to, amount);
+    }
+
+    function metaTransferHash(address to, uint256 amount, uint256 nonce) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(address(this), "metaTransfer", to, amount, nonce));
+    }
+
+    /**
+     * @dev Gets the signer of an hash using the signature
+     * @param hash The hash to check
+     * @param signature The signature to use
+     * @return The address of the signer or 0x0 address is something went wrong
+     */
+    function getSigner(bytes32 hash, bytes memory signature) public pure returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        if (signature.length != 65) {
+            return address(0);
+        }
+
+        /* solhint-disable-next-line no-inline-assembly */
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        if (v < 27) {
+            v += 27;
+        }
+
+        if (v != 27 && v != 28) {
+            return address(0);
+        } else {
+            return ecrecover(keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+            ), v, r, s);
+        }
     }
 }
--- test/Meerkoin.test.js
+++ test/Meerkoin.test.js
@@ -1,7 +1,9 @@
 /* eslint-env node, mocha */
 /* global artifacts, contract, it, assert */
 const Utils = require('web3-utils');
+const Accounts = require('web3-eth-accounts');
 
+const web3Accounts = new Accounts();
 const Meerkoin = artifacts.require('Meerkoin');
 
 let instance;
@@ -13,6 +15,11 @@ const token = {
   power: 10 ** 18,
   price: 0.01,
 };
+
+let nonce;
+let metaHash;
+const userPrivateKey = '0x9eff5cdfd780463812eeae4382c7575ade04fb07da4b5cc3416bafaf897f78a5';
+const userAddress = '0x86D2Fc11BE873ecA93A083c5cAbC38EC59bbC222';
 
 contract('Meerkoin', (accounts) => {
   it('Should deploy an instance of the Meerkoin contract', () => Meerkoin.deployed()
@@ -42,13 +49,70 @@ contract('Meerkoin', (accounts) => {
 
   it('Should buy some tokens', () => instance.buyTokens({
     from: accounts[0],
-    value: Utils.toWei('2'),
+    value: Utils.toWei('3'),
   }));
 
   it('Should check again the balance of account 0', () => instance.balanceOf(accounts[0])
     .then((balance) => {
-      const expectedBalance = 2 / token.price;
+      assert.equal(balance.toString(), Utils.toWei('3'), 'Balance is wrong');
+    }));
 
-      assert.equal(balance.toString(), Utils.toWei(expectedBalance.toString()), 'Balance is wrong');
+  it('Should sell some tokens from account 0', () => instance.sellTokens(
+    Utils.toWei('2'),
+  )
+    .then((tx) => {
+      const value = tx.receipt.logs[0].args.value.toString();
+      assert.equal(value, Utils.toWei('2'), 'Transferred value is wrong');
+    }));
+
+  it('Should check again the balance of account 0', () => instance.balanceOf(accounts[0])
+    .then((balance) => {
+      assert.equal(balance.toString(), Utils.toWei('1'), 'Balance is wrong');
+    }));
+
+  it('Should check the nonce for account 0', () => instance.nonces(accounts[0])
+    .then((res) => {
+      assert.equal(res, 0, 'Account 0 nonce is wrong');
+      nonce = res;
+    }));
+
+  it('Should get the hash for the metaTransfer function', () => instance.metaTransferHash(
+    accounts[1],
+    Utils.toWei('1'),
+    nonce,
+  )
+    .then((res) => {
+      const expectedMetaHash = Utils.soliditySha3(
+        instance.address,
+        'metaTransfer',
+        accounts[1],
+        Utils.toWei('1'),
+        nonce,
+      );
+
+      assert.equal(res, expectedMetaHash, 'Meta hash is wrong');
+      metaHash = res;
+    }));
+
+  it('Should sign and verify the signer of an hash', () => instance.getSigner(
+    metaHash,
+    web3Accounts.sign(metaHash, userPrivateKey).signature,
+  )
+    .then((res) => {
+      assert.equal(res, userAddress, 'Signer is wrong');
+    }));
+
+  it('Should use the metaTransfer function', () => instance.metaTransfer(
+    web3Accounts.sign(metaHash, userPrivateKey).signature,
+    accounts[1],
+    Utils.toWei('1'),
+    nonce, {
+      from: accounts[2],
+    },
+  ));
+
+  it('Should check account 1 balance', () => instance.balanceOf(accounts[1])
+    .then((balance) => {
+      assert.equal(balance, Utils.toWei('1'), 'Account 1 balance is wrong');
     }));
 });
